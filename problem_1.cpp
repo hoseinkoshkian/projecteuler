91d1590b41d96f2d721f41664fd6a3c3a68aab2b
ninja <sajjadkooshkian@gmail.com>
2023-10-17 20:14:34 +0330

    problem_1

---
+++ problem_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int sum;
+	for (int i=1 ; i<1000 ; i++)
+	{
+		if( (i%3==0)  or (i%5==0 ) )
+		{
+			cout<<i<<endl;
+			sum += i;
+		}
+	}
+
+	cout<<sum;
+}
---
+++ problem_1.py
@@ -0,0 +1,8 @@
+
+res = 0
+
+for i in range(1,1000):
+    if(  (i%3 == 0) | (i%5 == 0)  ):
+        res += i 
+
+print(res)
